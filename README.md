627ecbd20ecd1a93477e780295388c5d156b7f71
rizwanhkm <rizwan.hkm@gmail.com>
2015-03-15 22:21:42 +0530

    added column options generator

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
+A Script That allows you to add a search box to your site that Searches a Google SpreadSheet and displays the result as a table
+
 Depends on:
 ------------------
-
 1. jQuery
 2. jQuery-JSONP : https://github.com/jaubourg/jquery-jsonp
+
+Setup:
+---------
+spreadSheeData.setup take URL and IDs of required Elements and URL of the spreadSheet
+
+in index.html
+spreadSheetData.setup.url  = ''The URL of the spreadshee to be searched"
--- spreadSheetSearch.js
+++ spreadSheetSearch.js
@@ -1,65 +1,67 @@
-/*jslint browser: true*/
-var spreadSheetData = {};
+/*jslint browser: true */
+/*global $:false */
+var spreadSheetData = {
+    "setup": {
+        "url": "",
+        "msg": "",
+        "keyword": "",
+        "column": "",
+        "search": "",
+        "resulttable": "",
+        "startRow": "",
+        "startCol": ""
+    }
+};
+
 function parseData(entry) {
     "use strict";
     var i = 0,
-        row,
         entryLength = entry.length,
-        currentCellRow,
+        rowData = {},
+        numberOfColumns = 0,
+        row,
         col,
-        rowData;
+        currentCellRow;
+    if (!(spreadSheetData.setup.startRow)) {
+        spreadSheetData.setup.startRow = 1;
+    }
+    if (!(spreadSheetData.setup.startCol)) {
+        spreadSheetData.setup.startCol = 1;
+    }
+    for (i = 0; parseInt(entry[i].gs$cell.row, 10) < (spreadSheetData.setup.startRow + 1); i = i + 1) {
+        if (parseInt(entry[i].gs$cell.row, 10) === spreadSheetData.setup.startRow) {
+            numberOfColumns = numberOfColumns + 1;
+            rowData[numberOfColumns] = entry[i].content.$t;
+        }
+    }
+    spreadSheetData[0] = rowData;
+    spreadSheetData.columns = numberOfColumns;
     while (i < entryLength) {
-        row = parseInt(entry[i].gs$cell.row, 10);
-        currentCellRow = parseInt(entry[i].gs$cell.row, 10);
+        row = parseInt(entry[i].gs$cell.row - spreadSheetData.setup.startRow, 10);
+        currentCellRow = parseInt(entry[i].gs$cell.row - spreadSheetData.setup.startRow, 10);
         rowData = {};
-        while (row === currentCellRow && i < entryLength) {
+        while (currentCellRow === row && i < entryLength) {
             col = parseInt(entry[i].gs$cell.col, 10);
-            switch (col) {
-            case 1:
-                break;
-            case 2:
-                rowData.name = entry[i].content.$t;
-                break;
-            case 3:
-                rowData.email = entry[i].content.$t;
-                break;
-            case 4:
-                rowData.affiliation = entry[i].content.$t;
-                break;
-            case 5:
-                rowData.areasOfInterest = entry[i].content.$t;
-                break;
-            case 6:
-                rowData.position = entry[i].content.$t;
-                break;
-            case 7:
-                rowData.city = entry[i].content.$t;
-                break;
-            case 8:
-                rowData.country = entry[i].content.$t;
-                break;
-            default:
-                window.console.log("error Parsing Data " + entry[i]);
-                break;
-            }
+            rowData['"' + spreadSheetData[0][col] + '"'] = entry[i].content.$t;
             i = i + 1;
             if (i < entryLength) {
-                currentCellRow = parseInt(entry[i].gs$cell.row, 10);
+                currentCellRow = parseInt(entry[i].gs$cell.row - spreadSheetData.setup.startRow, 10);
             }
         }
         spreadSheetData[row] = rowData;
     }
-    spreadSheetData.length = row;
+    spreadSheetData.rows = row;
 }
+
 function findmatches(keyword, column) {
     "use strict";
     var regexp = new RegExp(keyword, "i"),
         i,
         counter = 0,
         results = {};
-    for (i = 2; i <= spreadSheetData.length; i = i + 1) {
+    for (i = 1; i <= spreadSheetData.rows; i = i + 1) {
         if (!(spreadSheetData[i] === undefined)) {
-            if (regexp.test(spreadSheetData[i][column])) {
+            if (regexp.test(spreadSheetData[i]['"' + column + '"'])) {
                 results[counter] = spreadSheetData[i];
                 counter = counter + 1;
             }
@@ -68,81 +70,83 @@ function findmatches(keyword, column) {
     results.length = counter;
     return results;
 }
+
 function printResults(results) {
     "use strict";
-    var searchresults = document.getElementById("spreadSheetSearchResults"),
-        i,
-        row,
-        name,
-        email,
-        affliation,
-        areasOfInterest,
-        position,
-        city,
-        country,
-        offset;
+    var i,
+        j,
+        row = "";
     if (results.length === 0) {
-        document.getElementById("spreadSheetSearchResultsMsg").innerHTML = "No Results Found";
-        offset = $("#spreadSheetSearchResultsMsg").offset();
-        offset.top -= 50;
-        $('html, body').animate({
-            scrollTop: offset.top
-        });
-        searchresults.innerHTML = "";
+        $("#" + spreadSheetData.setup.msg).html("No Results Found");
+        $("#" + spreadSheetData.setup.resulttable).find("tbody").empty();
     } else {
-        document.getElementById("spreadSheetSearchResultsMsg").innerHTML = "Total Results Found :" + results.length + "<br>Search Results:";
-        searchresults.innerHTML = "";
-        row = searchresults.insertRow(0);
-        name = row.insertCell(0);
-        email = row.insertCell(1);
-        affliation = row.insertCell(2);
-        areasOfInterest = row.insertCell(3);
-        position = row.insertCell(4);
-        city = row.insertCell(5);
-        country = row.insertCell(6);
-        name.innerHTML = "<div>" + spreadSheetData[1].name + "</div>";
-        email.innerHTML = "<div>" + spreadSheetData[1].email + "</div>";
-        affliation.innerHTML = "<div>" + spreadSheetData[1].affiliation + "</div>";
-        areasOfInterest.innerHTML = "<div>" + spreadSheetData[1].areasOfInterest + "</div>";
-        position.innerHTML = "<div>" + spreadSheetData[1].position + "</div>";
-        city.innerHTML = "<div>" + spreadSheetData[1].city + "</div>";
-        country.innerHTML = "<div>" + spreadSheetData[1].country + "</div>";
+        $("#" + spreadSheetData.setup.msg).html("Total Results Found :" + results.length + "<br>Search Results:");
+        $("#" + spreadSheetData.setup.resulttable).find("tbody").empty();
+        row = "<tr>";
+        for (i = spreadSheetData.setup.startCol; i <= spreadSheetData.columns; i = i + 1) {
+            row  += "<td><div>" + spreadSheetData[0][i] + "</div></td>";
+        }
+        row += "</tr>";
+        $("#" + spreadSheetData.setup.resulttable).append(row);
         for (i = 0; i < results.length; i = i + 1) {
-            row = searchresults.insertRow(i + 1);
-            name = row.insertCell(0);
-            email = row.insertCell(1);
-            affliation = row.insertCell(2);
-            areasOfInterest = row.insertCell(3);
-            position = row.insertCell(4);
-            city = row.insertCell(5);
-            country = row.insertCell(6);
-            name.innerHTML = "<div>" + results[i].name + "</div>";
-            email.innerHTML = "<div>" + results[i].email + "</div>";
-            affliation.innerHTML = "<div>" + results[i].affiliation + "</div>";
-            areasOfInterest.innerHTML = "<div>" + results[i].areasOfInterest + "</div>";
-            position.innerHTML = "<div>" + results[i].position + "</div>";
-            city.innerHTML = "<div>" + results[i].city + "</div>";
-            country.innerHTML = "<div>" + results[i].country + "</div>";
+            row = "<tr>";
+            for (j = spreadSheetData.setup.startCol; j <= spreadSheetData.columns; j = j + 1) {
+                row += "<td><div>" + results[i]['"' + spreadSheetData[0][j] + '"'] + "</div></td>";
+            }
+            row += "</tr>";
+            $("#" + spreadSheetData.setup.resulttable).append(row);
         }
     }
 }
+
 function search() {
     "use strict";
-    if (!(spreadSheetData.length)) {
-        document.getElementById("spreadSheetSearchResultsMsg").innerHTML = "Something went wrong :( ... Please Refresh the page ";
-        return;
-    }
     var searchparameters = {},
         results;
-    searchparameters.keyword = document.getElementById("spreadSheetSearchKeyword").value;
-    searchparameters.column = document.getElementById("spreadSheetSearchColumn").value;
+    searchparameters.keyword = $("#" + spreadSheetData.setup.keyword).val();
+    searchparameters.column = $("#" + spreadSheetData.setup.column).val();
     results = findmatches(searchparameters.keyword, searchparameters.column);
     printResults(results);
 }
+
+function populateColumn() {
+    "use strict";
+    var i, o;
+    for (i = spreadSheetData.setup.startCol; i <= spreadSheetData.columns; i = i + 1) {
+        o = new Option(spreadSheetData[0][i], spreadSheetData[0][i]);
+        $(o).html(spreadSheetData[0][i]);
+        $("#" + spreadSheetData.setup.column).append(o);
+    }
+}
+
 function spreadSheedSearchCallback(data) {
     "use strict";
     var entry = data.feed.entry;
     parseData(entry);
-    document.getElementById("spreadSheetSearch").style.visibility = "visible";
-//        console.log(spreadSheetData);
+    populateColumn();
+    $("#" + spreadSheetData.setup.search).css("visibility", "visible");
+    $("#" + spreadSheetData.setup.search).on("click", search);
+    $("#" + spreadSheetData.setup.keyword).on("keyup", search);
+    $(document).ready(function () {
+        $("#" + spreadSheetData.setup.keyword).keyup(function () {
+            var offset = $(this).offset();
+            offset.top -= 50;
+            $('html, body').animate({
+                scrollTop: offset.top
+            });
+        });
+    });
+}
+
+function spreadSheetSearchGetData() {
+    "use strict";
+    $.jsonp({
+        "url": spreadSheetData.setup.url + "?alt=json-in-script&callback=?",
+        "callback": "spreadSheedSearchCallback",
+        "success": spreadSheedSearchCallback,
+        "error": function () {
+            $("#" + spreadSheetData.setup.msg).innerHTML = "Something went wrong :( ... Please Refresh the page<br> ";
+            return;
+        }
+    });
 }
